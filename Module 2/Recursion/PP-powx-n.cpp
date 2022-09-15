a7624c67087b2ac74dd8aad6ee61865b3d18abf6
AdityaSrivastava <AdityaSrivastava.87260@gmail.com>
2022-09-15 13:16:16 +0530

    ok

---
+++ Module 2/Recursion/PP-powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double helper(double &x,int n)
+    {
+       if(n==0)
+           return 1;
+    
+        double t=helper(x,n/2);
+        if(n%2==0)
+        {if(n<0)
+            return 1/1.0*t*t;
+            return 1.0*t*t;
+        }
+        else
+        {if(n<0)
+            return 1/1.0*t*t*(1/x);
+            return 1.0*t*t*x;
+        }
+    }
+    double myPow(double x, int n) {
+        return helper(x,n);
+    }
+};
